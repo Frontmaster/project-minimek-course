2729d304e7df832a5330d80a5dcde5c36a6ed651
Oleksandr Bokhonko <Olexandr.Bokhonko@wolterskluwer.com>
2019-04-10 14:00:54 +0300

    Add initial Store setup

---
+++ src/store/configure-store.js
@@ -0,0 +1,19 @@
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root-reducer';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+
+export default function configureStore(preloadedState) {
+	const middlewares = [thunk];
+	const middlewareEnhancer = applyMiddleware(...middlewares);
+	const storeEnhancers = [middlewareEnhancer];
+	const composedEnhancer = composeWithDevTools(...storeEnhancers);
+
+	const store = createStore(
+		rootReducer,
+		preloadedState,
+		composedEnhancer
+	);
+
+	return store;
+}
